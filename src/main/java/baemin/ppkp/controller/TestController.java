a8edb00cbd28bb2dcf8a48a07bb82209e81c641f
Jo In Hyeok <ss112d86@gmail.com>
2023-06-20 22:28:19 +0900

    feat: controller 추가 (#2)
    * feat: controller 추가
    
    * feat: testController 추가

---
+++ src/main/java/baemin/ppkp/controller/TestController.java
@@ -0,0 +1,14 @@
+package baemin.ppkp.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("")
+    public String testing() {
+        return "test";
+    }
+}
